f7d43446e12d08b7fd2f71246da4867ec0640d4d
paulmunyao <paulmunyao094@gmail.com>
2023-02-27 15:17:42 +0300

    Initial commit

---
+++ promises.js
@@ -0,0 +1,59 @@
+// import fetch from "node-fetch";
+
+
+// var promise = new Promise(function(resolve,reject){
+//     const x ="Paul";
+//     const y ="Paul"
+//     if (x === y){
+//         resolve();
+//     }else{
+//         reject()
+//     }
+// });
+
+// promise.
+// then(function(){
+//     console.log("Successful");
+// }).
+// catch(function(){
+//     console.log ("Failed")
+// })
+
+// var promise = new Promise(function(resolve,reject){
+//     resolve("Paul")
+// });
+
+// promise.
+// then(function(successMessage){
+//     console.log(successMessage);
+// },function(errorMessage){
+//     console.log(errorMessage);
+// })
+
+// var promise = new Promise(function(resolve,reject){
+//     reject("Rejected")
+// });
+
+// promise.
+// then(function(successMessage){
+//     console.log(successMessage);
+// },function(errorMessage){
+//     console.log(errorMessage);
+// })
+
+// "https://fakestoreapi.com/products" - API endpoint
+
+
+// import fetch from "node-fetch"
+// fetch('https://example.com/data.txt')
+//   .then(response => response.text())
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(error => {
+//     console.error('Error fetching data:', error);
+//   });
+
+
+
+
